d15f104fa355abeff20b59391b4641b8ecc7c618
AlexandrNikolaev0 <roketka1@gmail.com>
2019-06-12 17:36:38 +0300

    Пример

---
+++ src/sample/RegistrationWindow.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegistrationWindow {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    void initialize() {
+
+    }
+}
---
+++ src/sample/StartWindow.java
@@ -0,0 +1,53 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StartWindow {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    void initialize() {
+        signUpButton.setOnAction(event -> {
+            signUpButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/registrationWindow.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+    }
+}
