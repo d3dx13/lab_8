c14e069d3f8b3c21c96f74e9d2cced21aa838082
AlexandrNikolaev0 <roketka1@gmail.com>
2019-06-13 16:03:54 +0300

    startwin

--- src/lab_8/client/ClientGUI/StartWindow.java
+++ src/lab_8/client/ClientGUI/StartWindow.java
@@ -3,13 +3,7 @@ package lab_8.client.ClientGUI;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 public class StartWindow {
 
@@ -20,61 +14,13 @@ public class StartWindow {
     private URL location;
 
     @FXML
-    private Button signUpButton;
-
-    @FXML
     private Button signInButton;
 
     @FXML
-    private Button chooseLoginButton;
-
-    @FXML
-    private Button helpButton;
-
-    @FXML
-    private Button chooseServerButton;
-
-    @FXML
-    private Label loginText;
-
-    @FXML
-    private Label addressText;
-
-    @FXML
-    private Label portText;
-
-    @FXML
-    private Button exitButton;
+    private Button signUpButton;
 
     @FXML
     void initialize() {
-        loginText.setText("Текущий логин: "+ClientData.login);
-        chooseLoginButton.setOnAction(event -> {
-            try {
-                Parent root = FXMLLoader.load(getClass().getResource("/lab_8/client/ClientGUI/loginWindow.fxml"));
-                Scene scene = new Scene(root);
-                Stage stage = new Stage();
-                stage.setScene(scene);
-                stage.show();
-                ((Node)(event.getSource())).getScene().getWindow().hide();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
 
-        addressText.setText("Адрес текущего сервера: \n"+ClientData.serverAddress);
-        portText.setText("Порт текущего сервера: \n");
-        chooseServerButton.setOnAction(event -> {
-            try {
-                Parent root = FXMLLoader.load(getClass().getResource("/lab_8/client/ClientGUI/Window.fxml"));
-                Scene scene = new Scene(root);
-                Stage stage = new Stage();
-                stage.setScene(scene);
-                stage.show();
-                ((Node)(event.getSource())).getScene().getWindow().hide();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
     }
 }
